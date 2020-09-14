e4272be81f8cff0b004489659fc9a4c33fcb23aa
Peter Schnabel <Peter@peters-mbp-3.lan>
2020-09-14 10:17:44 -0400

    api - change port to all upper case

--- api/src/config.ts
+++ api/src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  port: process.env.port || 5555,
+  port: process.env.PORT || 5555,
   clientName: process.env.mongoAtlasClientName,
   db: process.env.db || 'Maps',
   collection: process.env.collection || 'Track',
